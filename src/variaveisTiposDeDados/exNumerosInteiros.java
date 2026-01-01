abd77be24802ca7071c5bd862de672b1757c9321
Paulo Costa <pjrc83@gmail.com>
2026-01-01 12:43:25 -0300

    Aula6 exercício a aula concluída

---
+++ src/variaveisTiposDeDados/exNumerosInteiros.java
@@ -0,0 +1,28 @@
+package variaveisTiposDeDados;
+
+import java.util.Scanner;
+
+public class exNumerosInteiros {
+    public static void main(String[] args) {
+        //1. iniciando com a frase de efeito
+        Scanner sc = new Scanner(System.in);
+        System.out.println("VAMOS ATÉ A LUA HOJE!");
+        //2. Solicitar o peso do primeiro passageiro
+        //3. Armazenar o peso do passageiro 1 em variável
+        System.out.println("DIGITE O PESO DO PRIMEIRO PASSAGEIRO: ");
+        short peso1 = sc.nextShort();
+        //4. Solicitar o peso do segundo passageiro
+        //5. Armazenar o peso do passageiro 2 em variável
+        System.out.println("DIGITE O PESO DO SEGUNDO PASSAGEIRO: ");
+        short peso2 = sc.nextShort();
+        //6. Solicitar o peso do terceiro passageiro
+        //7. Armazenar o peso do passageiro 3 em variável
+        System.out.println("DIGITE O PESO DO TERCEIRO PASSAGEIRO: ");
+        short peso3 = sc.nextShort();
+        //8. Somar todos os pesos e armazenar em uma variável
+        int pesoTotal = (peso1 + peso2 + peso3);
+        //9. Viagem Liberada!
+        System.out.println("PESO TOTAL DOS PASSAGEIROS: "+pesoTotal+" KG");
+        System.out.println("VIAGEM LIBERADA");
+    }
+}
---
+++ src/variaveisTiposDeDados/numerosInteiros.java
@@ -0,0 +1,16 @@
+package variaveisTiposDeDados;
+
+public class numerosInteiros {
+    public static void main(String[] args) {
+        //Byte = -127 até 127
+        //Short = -32768 até 32767
+        //Int = -2147483648 até 2147483647
+        //Long = -9223372036854775808 até 9223372036854775807
+
+        // tipo nome = valor;
+
+        long meuNumero = -9223372036854775808L;
+
+
+    }
+}
