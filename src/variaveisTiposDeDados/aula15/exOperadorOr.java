63f884045fa3c67fae7b6b90a9ac0a26ca42e637
Paulo Costa <pjrc83@gmail.com>
2026-01-10 18:01:05 -0300

    Finalização da aula 15

--- src/variaveisTiposDeDados/aula15/exOperadorOr.java
+++ src/variaveisTiposDeDados/aula15/exOperadorOr.java
@@ -1,4 +1,27 @@
 package variaveisTiposDeDados.aula15;
 
+import javax.swing.*;
+import java.util.Scanner;
+
 public class exOperadorOr {
+    public static void main(String[] args) {
+        //1. Introdução
+        System.out.println("*****Antes de você assistir*****");
+        //2. Solicitar a idade
+        System.out.println("Qual a sua idade");
+        //3. Ler a idade
+        Scanner sc = new Scanner(System.in);
+        int idade = sc.nextInt();
+        sc.nextLine();
+        //4. Solicitar se ele tem permissão dos pais
+        System.out.println("Você tem permissão dos seus pais?");
+        //5. Ler resposta
+        String permissao = sc.nextLine();
+        //6. Verificar se ele pode assistir ou não
+        if (permissao.equalsIgnoreCase("sim") || idade > 17) {
+            System.out.println("Acesso liberado!");
+        }else{
+            System.out.println("Acesso negado!");
+        }
+    }
 }
