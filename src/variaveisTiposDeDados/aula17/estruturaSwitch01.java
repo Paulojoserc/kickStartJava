fb6ede888d81b2c1cf66cd4c74d7ab1afc32ec40
Paulojoserc <pjrc83@gmail.com>
2026-01-12 17:16:58 -0300

    Commit do da aula17 aula pratica

---
+++ src/variaveisTiposDeDados/aula17/estruturaSwitch01.java
@@ -0,0 +1,21 @@
+package variaveisTiposDeDados.aula17;
+
+public class estruturaSwitch01 {
+    public static void main(String[] args) {
+        //switch
+
+        int opcao = 0;
+        switch (opcao){
+
+            case 1:
+                System.out.println("Opção com valor de 1");
+                break;
+            case 2:
+                System.out.println("Opção com valor de 2");
+                break;
+            default:
+                System.out.println("Fluxo Padrão! nenhuma opção valida ! ");
+                break;
+        }
+    }
+}
---
+++ src/variaveisTiposDeDados/aula17/estruturaSwitch02.java
@@ -0,0 +1,30 @@
+package variaveisTiposDeDados.aula17;
+
+import java.util.Scanner;
+
+public class estruturaSwitch02 {
+    public static void main(String[] args) {
+        System.out.println("Digite uma opção");
+        System.out.println("1 - Conta corrente");
+        System.out.println("2 - Conta poupança");
+        System.out.println("Quer falar com um atendente? Digite qualuer numero diferente das opções acima");
+
+        Scanner sc = new Scanner(System.in);
+        int opcao = sc.nextInt();
+
+        switch (opcao){
+            case 1:
+                System.out.println("Menu conta corrente selecionado");
+                break;
+            case 2:
+                System.out.println("Menu conta poupança selecionado");
+                break;
+            default:
+                System.out.println("Falar com atendente selecionado");
+                break;
+        }
+        System.out.println("Redirecionando o cliente......");
+
+
+    }
+}
