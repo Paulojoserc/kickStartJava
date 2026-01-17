451661da7752f1191513dc1e7eaffa0a744448b8
Paulo Costa <pjrc83@gmail.com>
2026-01-16 23:30:27 -0300

    Aula 18 operador Ternario

---
+++ src/variaveisTiposDeDados/aula18/operadorTernario01.java
@@ -0,0 +1,11 @@
+package variaveisTiposDeDados.aula18;
+
+public class operadorTernario01 {
+    public static void main(String[] args) {
+        // condição logica ? verdadeiro : false
+
+        boolean condicao = true;
+
+    System.out.println(condicao ? "verdadeiro" : "false");
+    }
+}
---
+++ src/variaveisTiposDeDados/aula18/operadorTernario02.java
@@ -0,0 +1,14 @@
+package variaveisTiposDeDados.aula18;
+
+import java.util.Scanner;
+
+public class operadorTernario02 {
+    public static void main(String[] args) {
+        System.out.println("Digite a quantidade de itens que você comprou: ");
+        Scanner sc = new Scanner(System.in);
+        int qtdItens = sc.nextInt();
+        String percentualDesconto = qtdItens > 100 ? "10%" : "5%";
+        System.out.println("Você terá o desconto de "+percentualDesconto);
+
+    }
+}
