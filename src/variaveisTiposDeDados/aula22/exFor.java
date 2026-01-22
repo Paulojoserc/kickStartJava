8ca43f21390fa7b7313546d5d74dfece82484954
Paulo Costa <pjrc83@gmail.com>
2026-01-22 00:05:44 -0300

    Finalizando o modulo estrutura de repetição

--- src/variaveisTiposDeDados/aula22/exFor.java
+++ src/variaveisTiposDeDados/aula22/exFor.java
@@ -1,4 +1,26 @@
 package variaveisTiposDeDados.aula22;
 
+import java.util.Scanner;
+
 public class exFor {
+    public static void main(String[] args) {
+        System.out.println("##Desempenhos - Oracle##");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite a quantidade de membros");
+        int quantidadeMembros = sc.nextInt();
+        sc.nextLine();
+
+        int mediaTotal = 0;
+
+        for (int i = 1; i <= quantidadeMembros; i++) {
+            System.out.println("Digite a nota da membro " + i);
+            int notaMembro = sc.nextInt();
+            mediaTotal += notaMembro;
+        }
+        System.out.println("Pesquisa Finalizada!");
+        System.out.println("Gerando a media do time......");
+        System.out.println("O Desempenho médio do time É: " + mediaTotal / quantidadeMembros);
+        sc.close();
+    }
 }
---
+++ src/variaveisTiposDeDados/aula22/exForProfessor.java
@@ -0,0 +1,26 @@
+package variaveisTiposDeDados.aula22;
+
+import java.util.Scanner;
+
+public class exForProfessor {
+    public static void main(String[] args) {
+        //1. Introducao
+        System.out.println("## Desempenho - Oracle PeopleSoft ##");
+        //2. Perguntar a quantidade de membros dentro da equipe
+        System.out.println("Quantidade de membros dentro do time? ");
+        Scanner sc = new Scanner(System.in);
+        int qtdMembros = sc.nextInt();
+        sc.nextLine();
+        int quantidadeTotalDeNotas = 0;
+
+        //3. Perguntar a nota individual de cada membro
+        for (int c = 1; c <= qtdMembros; c++) {
+            //codigo a ser repetido
+            System.out.println("Digite a nota do membro "+c+" :");
+            quantidadeTotalDeNotas = quantidadeTotalDeNotas+sc.nextInt();
+        //4. Calcular a nota de desempenho médio do time
+            System.out.println("Gerando média do time......");
+            System.out.println("Resultado de desempenho médio: "+(quantidadeTotalDeNotas/qtdMembros));
+        }
+            }
+}
